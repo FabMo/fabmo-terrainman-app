8d5a32bc853d06fe7feecb7d508a921b92a4866a
Ryan Sturmer <ryansturmer@gmail.com>
2015-05-14 10:35:23 -0400

    Revise to use a single pass carving strategy

--- js/terrain.js
+++ js/terrain.js
@@ -7,8 +7,7 @@ Terrain.prototype.options = function(options) {
 	default_options = {
 		width : 5.0,
 		height : 5.0,
-		material_thickness : 1.0,
-		base_thickness : 1.0, 
+		cut_depth : 0.75,
 		method : 'simplex2',
 		resolution : 0.0625,
 		scale : 1.0,
@@ -23,7 +22,7 @@ Terrain.prototype.options = function(options) {
 	this.height = options.height || default_options.height;
 	this.material_thickness = options.material_thickness || default_options.material_thickness;
 	this.base_thickness = options.base_thickness || default_options.base_thickness;
-	this.cut_depth = this.material_thickness-this.base_thickness;
+	this.cut_depth = options.cut_depth;
 	this.method = options.method || default_options.method;
 	this.resolution = options.resolution || default_options.resolution;
 	this.scale = options.scale || default_options.scale;
@@ -53,49 +52,58 @@ Terrain.prototype.generateGCode = function(seed) {
 	var do_another_pass = true;
 	var res = this.resolution;
 	var scale = 1.0/this.scale;
-	var current_pass_depth = 0;
-	var gcodes = ['(Terrain File)','G0 Z' + this.zpullup.toFixed(3), 'G0 X0Y0', 'M4', 'G4 P3.0'];
+	var gcodes = ['(Terrain File)', 'G90', 'G20', 'G0 Z' + this.zpullup.toFixed(3), 'G0 X0Y0', 'M4', 'G4 P3.0'];
 
 	function move(x,y,z,fr) {
 		return "G1 X" + Number(x).toFixed(4) + " Y" + Number(y).toFixed(4) + " Z" + Number(z).toFixed(4) + " F" + (fr*60.0).toFixed(4);
 	};
 
-	var pass = 1;
+	// Do the initial plunge pass on the first row
+	var current_pass_depth = 0;
+	var do_another_pass = true;
+	var x = 0;
+	var y = 0;
+	var z = 0;
+	var dir = 1.0;
+	var pass = 0;
 	while(do_another_pass) {
-		// Start in the corner
-		var x = 0;
-		var y = 0;
-		var z = 0;
-		var dir = 1.0;
+		console.log('initial pass ' + pass)
 		do_another_pass = false;
-
-		// Set the pass depth
 		current_pass_depth -= this.pass_depth;
-
-		gcodes.push('(Pass #' + pass + ')');
-		gcodes.push('G0Z' + this.zpullup.toFixed(3));
-		gcodes.push('G0X0Y0');
-		// Iterate over rows, then columns
-		while(y <this.height) {
-			while((dir > 0) ? x < this.width : x > 0) {
-				// Calculate the Z (contour) at this point
-				var depth = -((this.cut_depth/2.0) + ((this.cut_depth/2.0)*noise.simplex2(scale*x, scale*y)));
-				if(current_pass_depth < depth) {
-					// If we bottom out, don't cut the pass depth, cut the actual contour of the terrain
-					z = depth;
-				} else {
-					// If we failed to bottom out ever, make sure to do another pass
-					z = current_pass_depth;
-					do_another_pass = true;
-				}
-				gcodes.push(move(x,y,z,this.feedrate))
-				x += dir*res;
+		while((dir > 0) ? x < this.width : x > 0) {
+			console.log('Moving x: ' + x)
+			var depth = -((this.cut_depth/2.0) + ((this.cut_depth/2.0)*noise.simplex2(scale*x, scale*y)));
+			if(current_pass_depth < depth) {
+				// If we bottom out, don't cut the pass depth, cut the actual contour of the terrain
+				z = depth;
+			} else {
+				// If we failed to bottom out ever, make sure to do another pass
+				z = current_pass_depth;
+				do_another_pass = true;
 			}
-			y += res;
-			dir = -dir;
+			gcodes.push(move(x,y,z,this.feedrate))
+			x += dir*res;
 		}
+		dir = -dir;
 		pass += 1;
 	}
+
+	y += res;
+
+//	gcodes.push('G0Z' + this.zpullup.toFixed(3));
+//	gcodes.push('G0X0Y0');
+	// Iterate over rows, then columns
+	while(y <this.height) {
+		while((dir > 0) ? x < this.width : x > 0) {
+			// Calculate the Z (contour) at this point
+			z = -((this.cut_depth/2.0) + ((this.cut_depth/2.0)*noise.simplex2(scale*x, scale*y)));
+			gcodes.push(move(x,y,z,this.feedrate))
+			x += dir*res;
+		}
+		y += res;
+		dir = -dir;
+	}
+
 	gcodes.push('G0Z' + this.zpullup.toFixed(3));
 	gcodes.push('M8');
 	gcodes.push('G0X0Y0');
